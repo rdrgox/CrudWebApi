2c482cf9343fbb24dd7ec83dab7c2dd04e180d85
rdrgox <rdrgox@gmail.com>
2020-01-13 20:20:47 -0300

    Conetexto nugget sqlserver design

---
+++ CrudWebApi/Data/AppDbContexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CrudWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebApi.Data
+{
+    public class AppDbContexto : DbContext
+    {
+        public AppDbContexto(DbContextOptions<AppDbContexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contacto> ContactoItems { get; set; }
+    }
+}
