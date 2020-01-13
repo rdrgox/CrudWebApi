f602b7d9ab136badfe628fbbe4266f7291afaee7
rdrgox <rdrgox@gmail.com>
2020-01-13 20:07:17 -0300

    clase contacto

---
+++ CrudWebApi/Models/Contacto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudWebApi.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public bool Verificado { get; set; }
+
+    }
+}
